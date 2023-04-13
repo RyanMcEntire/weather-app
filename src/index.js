06d9271c208f5d0d172bf728e91ca71dee945fcd
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-04-13 14:30:17 -0600

    add upgrade insecure requests to html meta tag

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './style.css';
 import { buildStructures, defaultInfoBuild } from './controller/build-page';
 
+document.getElementsByTagName(
+  'META'
+)[2].content = `http-equiv="Content-Security-Policy"   
+     content="upgrade-insecure-requests"`;
 document.body.appendChild(buildStructures());
-defaultInfoBuild()
+defaultInfoBuild();
